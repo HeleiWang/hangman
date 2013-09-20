d7ccffbb10e9cbd63f56c1820f9d01d59ccb7f2f
HeleiWang <wanghele@umich.edu>
2013-09-20 03:13:04 -0400

    follow the tu

--- hangu/src/com/example/hangu/MainActivity.java
+++ hangu/src/com/example/hangu/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.hangu;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
-
+	
+	public final static String EXTRA_MESSAGE = "com.example.hangu.MESSAGE";
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,4 +25,12 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {   	
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	EditText editText = (EditText) findViewById(R.id.edit_message);
+    	String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	System.out.print("11");
+    }
 }
